2c2b78f2e2d761fb29b3b14bafa93d4f274a05cb
Trevor Cickovski <movingpictures83@gmail.com>
2023-08-15 10:59:27 -0400

    Upgraded Quartus

--- DSwitch.v
+++ DSwitch.v
@@ -15,7 +15,7 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
-// CREATED		"Wed May 11 12:00:02 2022"
+// CREATED		"Tue Aug 15 10:56:57 2023"
 
 module DSwitch(
 	clk,
